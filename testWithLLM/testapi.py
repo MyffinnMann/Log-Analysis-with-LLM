99932f59dec853d70e69a787d1611699b4fe1221
OscarLofqvistLarsson <oscar.lofqvistlarsson@gmail.com>
2024-09-23 10:06:18 +0200

    kör på GPU

--- testWithLLM/testapi.py
+++ testWithLLM/testapi.py
@@ -4,24 +4,22 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import  OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
-from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 from langchain_huggingface import HuggingFaceEmbeddings
+import torch
 
 ollama_instance = Ollama(base_url = "http://127.0.0.1:11434", model = "llama3.1")
 
-#ollama_embedding = OllamaEmbeddings(model="llama3.1",show_progress=True)
-
-hf = HuggingFaceEmbeddings(show_progress = True)
+hf = HuggingFaceEmbeddings(show_progress = True, model_kwargs = {"device" : "cuda"})
 
 
-# log_file = Path(__file__).with_name("SSH.log")
+# log_file = Path(__file__).with_name("SSH.log") # 20mb
 # loader = TextLoader(log_file)
 
-prox_log = Path(__file__).with_name("Proxifier.log")
+prox_log = Path(__file__).with_name("Proxifier.log") # 2mb
 loader = TextLoader(prox_log)
 
-#test_log = Path(__file__).with_name("test.log")
+#test_log = Path(__file__).with_name("test.log") # 10 rader
 #loader = TextLoader(test_log)
 
 data = loader.load()
