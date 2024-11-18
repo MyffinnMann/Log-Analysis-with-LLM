c8fe66254e34269653db5a4e0f5b7281f2fc4031
ibab20 <ibab20@student.bth.se>
2024-11-18 11:18:09 +0100

    Session DB

--- backend/src/DB.py
+++ backend/src/DB.py
@@ -1,6 +1,9 @@
 
 import sqlite3
 from argon2 import PasswordHasher
+import random
+import string
+
 ph = PasswordHasher()
 
 def connect():
@@ -25,10 +28,19 @@ def check_login(username, pas):
     cursor = db.cursor()
     sql_query = """SELECT Hash_PWD FROM Password
                     WHERE user_ID = (SELECT ID FROM user WHERE Username = ?)"""
+    sql_query2 = """INSERT INTO Session (Session_ID, user_ID) VALUES (?, (SELECT ID FROM user WHERE Username = ?))"""
     try:
         cursor.execute(sql_query, (username,))
         stored_pas = cursor.fetchone()
         Bo_value = verify_pas(stored_pas[0], pas)
+        ## skapa session id om inloggningen lyckas
+        if Bo_value:
+            try:
+                session_id = generate_session_id()
+                cursor.execute(sql_query2, (session_id, username))
+                db.commit()
+            except:
+                print("Error inserting session id")
     except:
         return False
     return Bo_value
@@ -46,6 +58,15 @@ def create_tables():
         CREATE TABLE Password (
             ID INTEGER PRIMARY KEY AUTOINCREMENT,
             Hash_PWD TEXT NOT NULL,
+            user_ID INTEGER,
+            FOREIGN KEY(user_ID) REFERENCES user(ID)
+        );
+        ''')
+        ## skapade en till tabell för att randomiza session id
+        conn.execute('''
+        CREATE TABLE Session (
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            Session_ID TEXT NOT NULL,
             user_ID INTEGER,
             FOREIGN KEY(user_ID) REFERENCES user(ID)
         );
@@ -77,33 +98,39 @@ def insert_test_values():
         print("Test values already exists or error inserting test values")
     finally:
         conn.close()
+    
 
-def get_all_answer(username, token):
+## funktion för att mapa en session id till en användare
+def map_session_id_to_user(session_id):
     try:
         conn = connect()
         cursor = conn.cursor()
         cursor.execute('''
-        SELECT Question, ans FROM Question_Answers WHERE user_ID = (SELECT ID FROM user WHERE Username = ?);
-        ''', (username,))
-        rows = cursor.fetchall()
-        conn.close()
-        return rows
+        SELECT user_ID FROM Session WHERE Session_ID = ?;
+        ''', (session_id,))
+        user_id = cursor.fetchone()
+        return user_id[0]
     except:
-        print("error fetching answers")
+        print("Error mapping session id to user")
     finally:
         conn.close()
 
-def get_all_files(username):
+## funktion för att skapa 16 tecken+siffror lång session id
+def generate_session_id():
+    session_id = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+    return session_id
+
+## ta bort session id från databasen
+def remove_session_id(session_phrase):
     try:
         conn = connect()
         cursor = conn.cursor()
         cursor.execute('''
-        SELECT file_name, file_hash FROM Files WHERE user_ID = (SELECT ID FROM user WHERE Username = ?);
-        ''', (username,))
-        rows = cursor.fetchall()
-        conn.close()
-        return rows
+        DELETE FROM Session WHERE session_ID = ?;
+        ''', (session_phrase,))
+        conn.commit()
     except:
-        print("error fetching files")
+        print("Error removing session id")
     finally:
         conn.close()
+        
