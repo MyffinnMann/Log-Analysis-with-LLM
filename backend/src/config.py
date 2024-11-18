3c182a27dc6c1b340e48ab0c3c3af958e1ea6d90
OscarLofqvistLarsson <oscar.lofqvistlarsson@gmail.com>
2024-11-18 17:00:29 +0100

    ported main in modular to api

--- backend/src/config.py
+++ backend/src/config.py
@@ -10,4 +10,16 @@ use_cpu = True
 # Configure LLM
 model_name = "llama3.2"
 base_url = "http://127.0.0.1:11434"
-template = ""
+template = """
+    You are an AI assistant specialized in analyzing security issues from log files.
+    Follow these instructions: {chat_instruction}
+    Use the provided log file context to answer the user's current question.
+    Focus solely on the current question and avoid referencing previous interactions unless necessary.
+    Keep your response under 200 words.
+
+    {context}
+
+    User Question: {query}
+
+    Answer:
+    """
